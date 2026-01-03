8aaa0bd14c8d0f64c70c36bfb6bd5379f2c56990
Shivam <Shivamdombe1@gmail.com>
2026-01-03 20:52:40 +0530

    Fix Android build: Proactive resolution of all AndroidX/Support library conflicts

--- withAndroidXFix.js
+++ withAndroidXFix.js
@@ -21,9 +21,20 @@ allprojects {
             force 'androidx.core:core:1.16.0'
             force 'androidx.versionedparcelable:versionedparcelable:1.1.1'
             force 'androidx.annotation:annotation:1.9.1'
+            force 'androidx.appcompat:appcompat:1.7.0'
+            force 'androidx.fragment:fragment:1.8.6'
         }
         exclude group: 'com.android.support', module: 'support-compat'
+        exclude group: 'com.android.support', module: 'support-core-utils'
+        exclude group: 'com.android.support', module: 'support-core-ui'
+        exclude group: 'com.android.support', module: 'support-fragment'
+        exclude group: 'com.android.support', module: 'support-annotations'
+        exclude group: 'com.android.support', module: 'support-vector-drawable'
+        exclude group: 'com.android.support', module: 'animated-vector-drawable'
         exclude group: 'com.android.support', module: 'versionedparcelable'
+        exclude group: 'com.android.support', module: 'customview'
+        exclude group: 'com.android.support', module: 'localbroadcastmanager'
+        exclude group: 'com.android.support', module: 'loader'
     }
 }
 `;
